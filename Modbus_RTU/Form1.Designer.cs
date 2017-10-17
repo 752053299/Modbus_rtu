5d1a425ba3f63c230e520dbac17d0366c46aadeb
752053299 <752053299@qq.com>
2017-10-17 21:11:08 +0800

    初版

--- Modbus_RTU/Form1.Designer.cs
+++ Modbus_RTU/Form1.Designer.cs
@@ -29,11 +29,207 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.comboBoxPort = new System.Windows.Forms.ComboBox();
+            this.comboBoxParity = new System.Windows.Forms.ComboBox();
+            this.comboBoxDateBite = new System.Windows.Forms.ComboBox();
+            this.comboBoxBaud = new System.Windows.Forms.ComboBox();
+            this.comboBoxStopBit = new System.Windows.Forms.ComboBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.button2 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "端  口";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 61);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 12);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "波特率";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 91);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 12);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "数据位";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 125);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(41, 12);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "校验位";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(6, 155);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(41, 12);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "停止位";
+            // 
+            // comboBoxPort
+            // 
+            this.comboBoxPort.BackColor = System.Drawing.SystemColors.Window;
+            this.comboBoxPort.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxPort.FormattingEnabled = true;
+            this.comboBoxPort.Location = new System.Drawing.Point(53, 30);
+            this.comboBoxPort.Name = "comboBoxPort";
+            this.comboBoxPort.Size = new System.Drawing.Size(121, 20);
+            this.comboBoxPort.TabIndex = 5;
+            // 
+            // comboBoxParity
+            // 
+            this.comboBoxParity.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxParity.FormattingEnabled = true;
+            this.comboBoxParity.Items.AddRange(new object[] {
+            "None",
+            "Even",
+            "Odd",
+            "Mark",
+            "Space"});
+            this.comboBoxParity.Location = new System.Drawing.Point(53, 122);
+            this.comboBoxParity.Name = "comboBoxParity";
+            this.comboBoxParity.Size = new System.Drawing.Size(121, 20);
+            this.comboBoxParity.TabIndex = 6;
+            // 
+            // comboBoxDateBite
+            // 
+            this.comboBoxDateBite.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxDateBite.FormattingEnabled = true;
+            this.comboBoxDateBite.Items.AddRange(new object[] {
+            "5",
+            "6",
+            "7",
+            "8"});
+            this.comboBoxDateBite.Location = new System.Drawing.Point(53, 88);
+            this.comboBoxDateBite.Name = "comboBoxDateBite";
+            this.comboBoxDateBite.Size = new System.Drawing.Size(121, 20);
+            this.comboBoxDateBite.TabIndex = 7;
+            // 
+            // comboBoxBaud
+            // 
+            this.comboBoxBaud.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxBaud.FormattingEnabled = true;
+            this.comboBoxBaud.Items.AddRange(new object[] {
+            "9600",
+            "19200",
+            "38400",
+            "57600",
+            "115200"});
+            this.comboBoxBaud.Location = new System.Drawing.Point(53, 58);
+            this.comboBoxBaud.Name = "comboBoxBaud";
+            this.comboBoxBaud.Size = new System.Drawing.Size(121, 20);
+            this.comboBoxBaud.TabIndex = 8;
+            // 
+            // comboBoxStopBit
+            // 
+            this.comboBoxStopBit.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxStopBit.FormattingEnabled = true;
+            this.comboBoxStopBit.Items.AddRange(new object[] {
+            "1",
+            "1.5",
+            "2"});
+            this.comboBoxStopBit.Location = new System.Drawing.Point(53, 152);
+            this.comboBoxStopBit.Name = "comboBoxStopBit";
+            this.comboBoxStopBit.Size = new System.Drawing.Size(121, 20);
+            this.comboBoxStopBit.TabIndex = 9;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.comboBoxStopBit);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.comboBoxBaud);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.comboBoxDateBite);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.comboBoxParity);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.comboBoxPort);
+            this.groupBox1.Location = new System.Drawing.Point(21, 31);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(206, 184);
+            this.groupBox1.TabIndex = 10;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "串口设置";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(29, 261);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(191, 261);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 12;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(737, 551);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox comboBoxPort;
+        private System.Windows.Forms.ComboBox comboBoxParity;
+        private System.Windows.Forms.ComboBox comboBoxDateBite;
+        private System.Windows.Forms.ComboBox comboBoxBaud;
+        private System.Windows.Forms.ComboBox comboBoxStopBit;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button1;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- Modbus_RTU/Form1.cs
+++ Modbus_RTU/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO.Ports;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
@@ -11,9 +12,137 @@ namespace Modbus_RTU
 {
     public partial class Form1 : Form
     {
+        #region 串口私有属性
+        private string _portName;
+        #endregion
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        #region 控件初始化
+
+        /// <summary>
+        /// 窗口加载时
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LoadPortAttribute();
+            
+        }
+
+
+        /// <summary>
+        /// 加载默认设置
+        /// </summary>
+        private void LoadPortAttribute()
+        {
+
+            try
+            {
+                //端口号
+                String[] ports = SerialPort.GetPortNames();
+                if (ports.Length > 0)
+                {
+                    comboBoxPort.Items.Clear();
+                    comboBoxPort.Items.AddRange(ports); 
+                }
+
+                //波特率
+                comboBoxBaud.SelectedIndex = 4;//115200
+                //数据位
+                comboBoxDateBite.SelectedIndex = 3;//8
+                //校验位
+                comboBoxParity.SelectedIndex = 0; //None
+                //停止位
+                comboBoxStopBit.SelectedIndex = 0;//1
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                
+            }
+
+        }
+
+
+        #endregion
+   
+        /// <summary>
+        /// 串口打开
+        /// </summary>
+        private void startPort()
+        {
+
+                if (serialPort1 == null)
+                {
+                    serialPort1 = new SerialPort();
+                }
+
+            try
+            {
+                serialPort1.PortName = comboBoxPort.Text;
+                serialPort1.BaudRate = Convert.ToInt32(comboBoxBaud.Text);
+                serialPort1.DataBits = Convert.ToInt32(comboBoxDateBite.Text);
+
+
+                switch (comboBoxStopBit.SelectedIndex)
+                {
+                    case 0:
+                        serialPort1.StopBits = StopBits.One;
+                        break;
+                    case 1:
+                        serialPort1.StopBits = StopBits.OnePointFive;
+                        break;
+                    case 2:
+                        serialPort1.StopBits = StopBits.Two;
+                        break;
+                    default:
+                        break;
+                }
+
+                switch (comboBoxParity.SelectedIndex)
+                {
+                    case 0:
+                        serialPort1.Parity = Parity.None;
+                        break;
+                    case 1:
+                        serialPort1.Parity = Parity.Even;
+                        break;
+                    case 2:
+                        serialPort1.Parity = Parity.Odd;
+                        break;
+                    case 3:
+                        serialPort1.Parity = Parity.Mark;
+                        break;
+                    case 4:
+                        serialPort1.Parity = Parity.Space;
+                        break;
+                    default:
+                        break;
+                }
+
+
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);     
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            Console.WriteLine("串口是否打开：" + serialPort1.IsOpen);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            startPort();
         }
     }
 }
